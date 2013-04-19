0d1cdccf76186d445c9e36e386ec208b4cbd71ac
Mike Stark <mike.stark@vidyard.com>
2013-04-19 11:50:06 -0400

    Add post req

--- server.js
+++ server.js
@@ -1,12 +1,21 @@
 var express = require("express");
 var app = express();
-app.use(express.logger());
+
+app.configure(function(){
+  app.use(express.logger());
+  app.use(express.bodyParser());
+});
 
 app.get('/', function(req,res){
   console.log(req);
   res.send("Hello World");
 });
 
+app.post('/', function(req,res){
+  console.log("POST to SERVER");
+  console.log(req.body);
+  res.send("ok");
+});
 var port = process.env.PORT || 808;
 app.listen(port, function(){
   console.log("Listening on " + port );
