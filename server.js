cdac732e6619cf36aa9ac6a81065d9c33d4a7bc5
Mike Stark <mike.stark@vidyard.com>
2013-04-19 11:39:02 -0400

    Init

--- server.js
+++ server.js
@@ -4,9 +4,10 @@ app.use(express.logger());
 
 app.get('/', function(req,res){
   console.log(req);
+  res.send("Hello World");
 });
 
-var port = process.env.PORT || 8080;
+var port = process.env.PORT || 808;
 app.listen(port, function(){
   console.log("Listening on " + port );
 });
