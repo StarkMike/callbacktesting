1ec254534193dce7a507d2ab804cb3b76c710f38
Mike Stark <mike.stark@vidyard.com>
2013-06-21 10:50:57 -0400

    use socket.io for updating

--- server.js
+++ server.js
@@ -1,22 +1,32 @@
 var express = require("express");
 var app = express();
+var http = require('http');
+var server = http.createServer(app)
+var io = require('socket.io').listen(server);
 
 app.configure(function(){
+  app.set('views', __dirname + '/views');
+  app.set('view engine', 'jade');
   app.use(express.logger());
   app.use(express.bodyParser());
 });
 
 app.get('/', function(req,res){
-  console.log("GET");
-  res.send("Hello World");
+  res.render('index');
 });
 
 app.post('/', function(req,res){
-  console.log("POST to SERVER");
-  console.log(req.body);
+  //POST data to any listeners
+  io.sockets.in('callbackroom').emit('message', JSON.stringify(req.body));
   res.send("ok");
 });
+
 var port = process.env.PORT || 8000;
-app.listen(port, function(){
+server.listen(port, function(){
   console.log("Listening on " + port );
+});
+
+//Socket events
+io.on('connection', function(socket){
+  socket.join('callbackroom');
 });
